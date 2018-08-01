32099c911679aacc3d335052530695999ea9b739
Alexey Makarov <mailto:bassbass@yandex.ru>
2018-08-01 12:48:00 +0300

    6.5. Создать программу для сортировки массива методом перестановки.[#64279]

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+/**
+ * Класс для сортировки массива с помощью алгоритма сортировки пузырьком.
+ * @author Alexey Makarov
+ * @since 01.08.18
+ * @version 0.1
+ */
+public class BubbleSort {
+
+    /**
+     * Метод сортировки массива.
+     * @param array массив, который нужно отсортировать.
+     * @return отсортированный массив.
+     */
+    int[] sort(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+/**
+ * JUnit класс для теста класса ArrayChar.
+ * @author Alexey Makarov
+ * @since 01.08.2018
+ * @version 0.1
+ */
+public class BubbleSortTest {
+
+    /**
+     * Шесть элементов в массиве.
+     */
+    @Test
+    public void whenSixElementsInArray() {
+        int[] input = {6, 2, 46, 10, 18, 70};
+        int[] answer = {2, 6, 10, 18, 46, 70};
+        BubbleSort bs = new BubbleSort();
+        assertThat(bs.sort(input), is(answer));
+    }
+}
