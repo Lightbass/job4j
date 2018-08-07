27c0c940c976bfec70ec5563a0b7c5f6f07e382e
Alexey <bassbass@yandex.ru>
2018-08-07 14:28:58 +0300

    Исправление: 1. Реализовать события на внутренних классах.[#65163]

--- chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -19,7 +19,7 @@ public class MenuTracker {
      * @since 07.08.2018
      * @version 0.1
      */
-    class EditItem implements UserAction {
+    private class EditItem implements UserAction {
 
         private final int key;
         private final String info;
@@ -67,7 +67,7 @@ public class MenuTracker {
          */
         @Override
         public String info() {
-            return info;
+            return String.format("%d %s", this.key(), info);
         }
     }
 
@@ -77,7 +77,7 @@ public class MenuTracker {
      * @since 07.08.2018
      * @version 0.1
      */
-    class DeleteItem implements UserAction {
+    private class DeleteItem implements UserAction {
 
         private final int key;
         private final String info;
@@ -122,7 +122,281 @@ public class MenuTracker {
          */
         @Override
         public String info() {
-            return info;
+            return String.format("%d %s", this.key(), info);
+        }
+    }
+
+    /**
+     * Класс для действия - отображение всех заявок.
+     * @author Alexey Makarov
+     * @since 07.08.2018
+     * @version 0.1
+     */
+    private class ShowItems implements UserAction {
+
+        private final int key;
+        private final String info;
+
+        /**
+         * Конструктор инициализирует информацию о данном действии и его ключ.
+         * @param key ключ
+         * @param info информация.
+         */
+        public ShowItems(int key, String info) {
+            this.key = key;
+            this.info = info;
+        }
+
+        /**
+         * Метод возвращает ключ опции.
+         * @return ключ
+         */
+        @Override
+        public int key() {
+            return key;
+        }
+
+        /**
+         * Основной метод.
+         * @param input объект типа Input
+         * @param tracker объект типа Tracker
+         */
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println(LS + "------------ Заявки в трекере --------------" + LS);
+            Item[] items = tracker.findAll();
+            for (Item i : items) {
+                System.out.println(i + LS);
+            }
+            System.out.println("------------ Перечисление завершено --------------");
+        }
+
+        /**
+         * Информация об операции AddAction.
+         * @return строка с информацией.
+         */
+        @Override
+        public String info() {
+            return String.format("%d %s", this.key(), info);
+        }
+    }
+
+    /**
+     * Класс для действия - поиск заявки по имени.
+     * @author Alexey Makarov
+     * @since 07.08.2018
+     * @version 0.1
+     */
+    private class FindItemsByName implements UserAction {
+
+        private final int key;
+        private final String info;
+
+        /**
+         * Конструктор инициализирует информацию о данном действии и его ключ.
+         * @param key ключ
+         * @param info информация.
+         */
+        public FindItemsByName(int key, String info) {
+            this.key = key;
+            this.info = info;
+        }
+
+        /**
+         * Метод возвращает ключ опции.
+         * @return ключ
+         */
+        @Override
+        public int key() {
+            return key;
+        }
+
+        /**
+         * Основной метод.
+         * @param input объект типа Input
+         * @param tracker объект типа Tracker
+         */
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println(LS + "------------ Поиск заявок по имени --------------" + LS);
+            String findName = input.ask("Введите искомое имя заявки: ");
+            Item[] items = tracker.findByName(findName);
+            if (items.length == 0) {
+                System.out.println(LS + "------------ Заявок с таким именем не существует --------------");
+                return;
+            }
+            for (Item i : items) {
+                System.out.println(LS + i);
+            }
+            System.out.println(LS + "------------ Перечисление завершено --------------");
+        }
+
+        /**
+         * Информация об операции AddAction.
+         * @return строка с информацией.
+         */
+        @Override
+        public String info() {
+            return String.format("%d %s", this.key(), info);
+        }
+    }
+
+    /**
+     * Класс для действия - поиск заявки по идентификатору.
+     * @author Alexey Makarov
+     * @since 07.08.2018
+     * @version 0.1
+     */
+    private class FindItemById implements UserAction {
+
+        private final int key;
+        private final String info;
+
+        /**
+         * Конструктор инициализирует информацию о данном действии и его ключ.
+         * @param key ключ
+         * @param info информация.
+         */
+        public FindItemById(int key, String info) {
+            this.key = key;
+            this.info = info;
+        }
+
+        /**
+         * Метод возвращает ключ опции.
+         * @return ключ
+         */
+        @Override
+        public int key() {
+            return key;
+        }
+
+        /**
+         * Основной метод.
+         * @param input объект типа Input
+         * @param tracker объект типа Tracker
+         */
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println(LS + "------------ Поиск заявки по идентификатору --------------" + LS);
+            Item item = checkStrGetItem(input.ask("Введите идентификатор(ID) заявки : "), tracker);
+            if (item != null) {
+                System.out.println(LS + item + LS + LS + "------------ Найдена заявка -----------");
+            }
+        }
+
+        /**
+         * Информация об операции AddAction.
+         * @return строка с информацией.
+         */
+        @Override
+        public String info() {
+            return String.format("%d %s", this.key(), info);
+        }
+    }
+
+    /**
+     * Класс для действия - выход из программы.
+     * @author Alexey Makarov
+     * @since 07.08.2018
+     * @version 0.1
+     */
+    private class ExitProgram implements UserAction {
+
+        private final int key;
+        private final String info;
+
+        /**
+         * Конструктор инициализирует информацию о данном действии и его ключ.
+         * @param key ключ
+         * @param info информация.
+         */
+        public ExitProgram(int key, String info) {
+            this.key = key;
+            this.info = info;
+        }
+
+        /**
+         * Метод возвращает ключ опции.
+         * @return ключ
+         */
+        @Override
+        public int key() {
+            return key;
+        }
+
+        /**
+         * Основной метод.
+         * @param input объект типа Input
+         * @param tracker объект типа Tracker
+         */
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println(LS + "------------ Выход из программы --------------" + LS);
+        }
+
+        /**
+         * Информация об операции AddAction.
+         * @return строка с информацией.
+         */
+        @Override
+        public String info() {
+            return String.format("%d %s", this.key(), info);
+        }
+    }
+
+    /**
+     * Класс для действия - добавление новой заявки.
+     * @author Alexey Makarov
+     * @since 07.08.2018
+     * @version 0.1
+     */
+    private class AddItem implements UserAction {
+
+        private final int key;
+        private final String info;
+
+        /**
+         * Конструктор инициализирует информацию о данном действии и его ключ.
+         * @param key ключ
+         * @param info информация.
+         */
+        public AddItem(int key, String info) {
+            this.key = key;
+            this.info = info;
+        }
+
+        /**
+         * Метод возвращает ключ опции.
+         * @return ключ
+         */
+        @Override
+        public int key() {
+            return key;
+        }
+
+        /**
+         * Основной метод.
+         * @param input объект типа Input
+         * @param tracker объект типа Tracker
+         */
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println(LS + "------------ Добавление новой заявки --------------" + LS);
+            String name = input.ask("Введите имя заявки : ");
+            String desc = input.ask("Введите описание заявки : ");
+            Item item = new Item(name, desc);
+            tracker.add(item);
+            System.out.println(LS + item + LS + LS + "------------ Новая заявка создана -----------");
+        }
+
+        /**
+         * Информация об операции AddAction.
+         * @return строка с информацией.
+         */
+        @Override
+        public String info() {
+            return String.format("%d %s", this.key(), info);
         }
     }
 
@@ -165,8 +439,8 @@ public class MenuTracker {
     public void fillActions() {
         this.actions.add(new AddItem(0, "0. Добавить новую заявку"));
         this.actions.add(new ShowItems(1, "1. Показать все заявки"));
-        this.actions.add(new MenuTracker.EditItem(2, "2. Редактировать заявку"));
-        this.actions.add(new MenuTracker.DeleteItem(3, "3. Удалить заявку"));
+        this.actions.add(new EditItem(2, "2. Редактировать заявку"));
+        this.actions.add(new DeleteItem(3, "3. Удалить заявку"));
         this.actions.add(new FindItemById(4, "4. Найти заявку по идентификатору(id)"));
         this.actions.add(new FindItemsByName(5, "5. Найти заявку по имени"));
         this.actions.add(new ExitProgram(6, "6. Выход из программы"));
