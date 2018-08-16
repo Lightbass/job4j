c093b3851a61d8b50f81915e88eb5927b1e6ff6c
Alexey <bassbass@yandex.ru>
2018-08-16 13:20:41 +0300

    5.1.1. Итератор для двухмерного массива int[][][#66657]

---
+++ chapter_101/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,59 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Класс для получения итератора из массива.
+ * @author Alexey Makarov
+ * @since 15.08.18
+ * @version 0.1
+ */
+public class MatrixIterator implements Iterator {
+    private int[][] mas;
+    private int posi;
+    private int posj;
+
+    /**
+     * Конструктор инициализирующий двумерный массив, который будет перебираться.
+     * @param e двумерный массив.
+     */
+    public MatrixIterator(int[][] e) {
+        this.mas = e;
+    }
+
+    /**
+     * Метод проверяет, есть ли в итераторе следующий элемент.
+     * @return есть или нет.
+     */
+    @Override
+    public boolean hasNext() {
+        boolean result = false;
+        if (mas.length != 0) {
+            result = true;
+            while (posj == mas[posi].length) {
+                if (posi != mas.length - 1) {
+                    posi++;
+                    posj = 0;
+                } else {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод возвращает следующий элемент.
+     * @return элемент.
+     */
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return mas[posi][posj++];
+    }
+}
---
+++ chapter_101/src/test/java/ru/job4j/iterator/JaggedMatrixIteratorTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * JUnit тест класса MatrixIterator.
+ * @author Alexey Makarov
+ * @since 10.08.18
+ * @version 0.1
+ */
+public class JaggedMatrixIteratorTest {
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new MatrixIterator(new int[][]{{1}, {3, 4}, {7}});
+    }
+
+    @Test
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    @Test
+    public void testsThatIteratorMissedEmptyArrays() {
+        it = new MatrixIterator(new int[][]{{1}, {3, 4}, {}, {7}});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+}
