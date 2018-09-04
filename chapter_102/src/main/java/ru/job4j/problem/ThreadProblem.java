1b2e36fafb07e724a8cac05135ba33439aaed7dc
Alexey <bassbass@yandex.ru>
2018-09-04 12:23:48 +0300

    1. Проиллюстрировать проблемы с многопоточностью.[#69578]

--- chapter_102/src/main/java/ru/job4j/problem/ThreadProblem.java
+++ chapter_102/src/main/java/ru/job4j/problem/ThreadProblem.java
@@ -6,5 +6,29 @@ package ru.job4j.problem;
  * @since 03.09.18
  * @version 0.1
  */
-public class ThreadProblem {
+public class ThreadProblem implements Runnable {
+    private int count;
+    private Increment inc;
+
+    public ThreadProblem(int count, Increment inc) {
+        this.count = count;
+        this.inc = inc;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < count; i++) {
+            inc.inc();
+        }
+    }
+}
+
+class Increment {
+    private volatile int i;
+    public void inc() {
+        i++;
+    }
+    public int get() {
+        return i;
+    }
 }
---
+++ chapter_102/src/test/java/ru/job4j/problem/ThreadProblemTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.problem;
+
+import javafx.scene.shape.Rectangle;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * JUnit тест класса ThreadProblem.
+ * @author Alexey Makarov
+ * @since 04.09.18
+ * @version 0.1
+ */
+public class ThreadProblemTest {
+
+    @Test
+    public void when2ThreadsIncrementSameValueThenFailed() {
+        Increment inc = new Increment();
+        ThreadProblem problem = new ThreadProblem(10000000, inc);
+        Thread thread1 = new Thread(problem);
+        Thread thread2 = new Thread(problem);
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        System.out.println(inc.get());
+        assertThat(inc.get() == 20000000, is(false));
+    }
+}
