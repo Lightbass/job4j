24ca95cd557e225827fc5456468d570bbe0cf09d
Lightbass <bassbass@yandex.ru>
2021-07-06 13:32:36 +0300

    3.1.1. Threads 1.1. Состояние нити. [#229175]

---
+++ chapter_311_threads/src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Runnable runnable = () -> {};
+        Thread first = new Thread(runnable);
+        Thread second = new Thread(runnable);
+        printTwoThreadInfo(first, second);
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            printTwoThreadInfo(first, second);
+        }
+        printTwoThreadInfo(first, second);
+    }
+
+    private static void printTwoThreadInfo(Thread thread1, Thread thread2) {
+        System.out.println("Thread name: " + thread1.getName() + ", state: " + thread1.getState());
+        System.out.println("Thread name: " + thread2.getName() + ", state: " + thread2.getState());
+    }
+}
