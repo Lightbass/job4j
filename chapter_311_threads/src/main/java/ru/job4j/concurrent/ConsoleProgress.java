a72a3bd9f82a9120d113ccaf90f96ef3f8f1ff6c
Lightbass <bassbass@yandex.ru>
2021-08-21 19:01:48 +0300

    3.1.1. Threads 3. Прерывание нити [#1019]

---
+++ chapter_311_threads/src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        char[] process = {'-', '\\', '|', '/'};
+        int count = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r Loading: " + process[count]);
+            count = count == 3 ? 0 : count + 1;
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+        System.out.print("\r Loaded");
+    }
+}
