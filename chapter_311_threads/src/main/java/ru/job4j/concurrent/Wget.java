857ba1caa1bdc9765970db53b0ca8206dca90394
Lightbass <bassbass@yandex.ru>
2021-11-28 16:09:19 +0300

    3.1.1. Threads 4. Скачивание файла с ограничением. [#144271]

--- chapter_311_threads/src/main/java/ru/job4j/concurrent/Wget.java
+++ chapter_311_threads/src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,25 +1,58 @@
 package ru.job4j.concurrent;
 
-import java.util.stream.IntStream;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-            IntStream.rangeClosed(0, 100)
-                    .forEach(progress -> {
-                        System.out.print("\rLoading: " + progress + "%");
-                        simulationOfWork();
-                    });
-            System.out.print("\rLoaded.");
-        });
-        thread.start();
+/**
+ * Класс для скачивания файла с ограниченной скоростью.
+ *
+ * @author Alexey Makarov
+ * @version 0.1
+ */
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    /**
+     * Инициализация объекта класса.
+     * @param url ссылка по которой будет скачан файл.
+     * @param speed скорость в килобайтах в секунду.
+     */
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
     }
 
-    private static void simulationOfWork() {
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            int totalBytesRead = 0;
+            long beginTime = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                totalBytesRead += bytesRead;
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                if (totalBytesRead > speed * 1024) {
+                    System.out.print("\rDownloading: " + totalBytesRead + " bytes/sec ");
+                    totalBytesRead = 0;
+                    Thread.sleep(1000 - (System.currentTimeMillis() - beginTime));
+                    beginTime = System.currentTimeMillis();
+                }
+            }
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
