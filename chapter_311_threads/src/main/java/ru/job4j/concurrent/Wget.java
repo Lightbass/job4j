b5b0d813a6cd4d5682f4b57e489ebea4171bfe02
Lightbass <bassbass@yandex.ru>
2021-12-19 15:48:43 +0300

    3.1.1. Threads 4. Скачивание файла с ограничением. [#144271] - fix bugs

--- chapter_311_threads/src/main/java/ru/job4j/concurrent/Wget.java
+++ chapter_311_threads/src/main/java/ru/job4j/concurrent/Wget.java
@@ -14,21 +14,23 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String fileName;
 
     /**
      * Инициализация объекта класса.
      * @param url ссылка по которой будет скачан файл.
      * @param speed скорость в килобайтах в секунду.
      */
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             int totalBytesRead = 0;
@@ -39,7 +41,10 @@ public class Wget implements Runnable {
                 if (totalBytesRead > speed * 1024) {
                     System.out.print("\rDownloading: " + totalBytesRead + " bytes/sec ");
                     totalBytesRead = 0;
-                    Thread.sleep(1000 - (System.currentTimeMillis() - beginTime));
+                    long sleepTime = 1000 - (System.currentTimeMillis() - beginTime);
+                    if (sleepTime > 0) {
+                        Thread.sleep(1000 - (System.currentTimeMillis() - beginTime));
+                    }
                     beginTime = System.currentTimeMillis();
                 }
             }
@@ -49,10 +54,24 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (!validate(args)) {
+            System.out.println("Please enter valid arguments: speed(int) link(String) outputFileName(String)");
+            return;
+        }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String fileName = args.length > 2 ? args[2] : "downloadedFile";
+        Thread wget = new Thread(new Wget(url, speed, fileName));
         wget.start();
         wget.join();
+    }
+
+    private static boolean validate(String[] args) {
+        boolean result = true;
+        String urlValidate = "https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)";
+        if (args.length < 2 || !args[0].matches(urlValidate) || !args[1].matches("^\\d+$")) {
+            result = false;
+        }
+        return result;
     }
 }
