c792bd2d81c85cf2d8e1aa868d504403d8691f09
Lightbass <bassbass@yandex.ru>
2022-01-16 16:47:04 +0300

    3.1.1. Threads 4. Скачивание файла с ограничением. [#144271] - fix code after review

--- chapter_311_threads/src/main/java/ru/job4j/concurrent/Wget.java
+++ chapter_311_threads/src/main/java/ru/job4j/concurrent/Wget.java
@@ -3,7 +3,9 @@ package ru.job4j.concurrent;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Paths;
 
 /**
  * Класс для скачивания файла с ограниченной скоростью.
@@ -12,9 +14,12 @@ import java.net.URL;
  * @version 0.1
  */
 public class Wget implements Runnable {
+
     private final String url;
     private final int speed;
     private final String fileName;
+
+    private static final String FILENAME_HEADER = "filename=";
 
     /**
      * Инициализация объекта класса.
@@ -30,20 +35,20 @@ public class Wget implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+             FileOutputStream fileOutputStream = new FileOutputStream(getFilename(url))) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             int totalBytesRead = 0;
             long beginTime = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                totalBytesRead += bytesRead;
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
+                totalBytesRead += bytesRead;
                 if (totalBytesRead > speed * 1024) {
                     System.out.print("\rDownloading: " + totalBytesRead + " bytes/sec ");
                     totalBytesRead = 0;
                     long sleepTime = 1000 - (System.currentTimeMillis() - beginTime);
                     if (sleepTime > 0) {
-                        Thread.sleep(1000 - (System.currentTimeMillis() - beginTime));
+                        Thread.sleep(sleepTime);
                     }
                     beginTime = System.currentTimeMillis();
                 }
@@ -55,15 +60,32 @@ public class Wget implements Runnable {
 
     public static void main(String[] args) throws InterruptedException {
         if (!validate(args)) {
-            System.out.println("Please enter valid arguments: speed(int) link(String) outputFileName(String)");
-            return;
+            throw new IllegalArgumentException("Please enter valid arguments: speed(int kb/sec) link(String name) outputFileName(String name)");
         }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        String fileName = args.length > 2 ? args[2] : "downloadedFile";
+        String fileName = args.length > 2 ? args[2] : null;
         Thread wget = new Thread(new Wget(url, speed, fileName));
         wget.start();
         wget.join();
+    }
+
+    private String getFilename(String url) throws MalformedURLException {
+        if (fileName != null) {
+            return fileName;
+        }
+        URL urlObject = new URL(url);
+        String filename = urlObject.getQuery();
+        int filenameIndex = filename == null ? -1 : filename.indexOf(FILENAME_HEADER);
+        if (filenameIndex != -1) {
+            filename = filename.substring(filenameIndex + FILENAME_HEADER.length());
+            if (filename.contains("&")) {
+                filename = filename.substring(0, filename.indexOf('&'));
+            }
+        } else {
+            filename = Paths.get(urlObject.getPath()).getFileName().toString();
+        }
+        return filename;
     }
 
     private static boolean validate(String[] args) {
